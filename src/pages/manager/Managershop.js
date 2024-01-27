d6da18592ca22cc76c38dd48a2fa41e711940dca
gonookim <gonoo1414@gmail.com>
2024-01-27 13:23:19 +0900

    feat:알람창 및 404페이지 수정

--- src/pages/manager/Managershop.js
+++ src/pages/manager/Managershop.js
@@ -10,36 +10,41 @@ export default function Managershop(){
                 <div className="w-1/3 h-fit mx-auto my-28">
 
                     <h1 className="text-5xl font-semibold mx-auto w-fit text-lime-700" style={{fontFamily: 'SUITE-Regular'}}>SSGTARBUCKS</h1>
-                    
+
                     <div className="flex h-20 justify-around items-center rounded-2xl shadow-lg my-4" style={{backgroundColor:"#f6f5efb3", border:"1px solid #d5d5d5"}}>
                         <h4 className="text-2xl  text-lime-700" style={{fontFamily: 'SUITE-Regular'}}>
-                            <label for="e-mail">e-mail</label>
+                            <label for="e-mail">직원유형</label>
                         </h4>
                         <input className="w-64 h-10" style={{border:"1px solid #d5d5d5"}} disabled type="e-mail" id="e-mail"></input>
                     </div>
                     <div className="flex h-20 justify-around items-center rounded-2xl shadow-lg my-4" style={{backgroundColor:"#f6f5efb3", border:"1px solid #d5d5d5"}}>
                         <h4 className="text-2xl  text-lime-700" style={{fontFamily: 'SUITE-Regular'}}>
-                            <label for="number">사원번호</label>
+                            <label for="e-mail">휴대번호</label>
                         </h4>
-                        <input className="w-64 h-10" style={{border:"1px solid #d5d5d5"}} disabled type="text" id="number"></input>
+                        <input className="w-64 h-10" style={{border:"1px solid #d5d5d5"}} disabled type="e-mail" id="e-mail"></input>
                     </div>
                     <div className="flex h-20 justify-around items-center rounded-2xl shadow-lg my-4" style={{backgroundColor:"#f6f5efb3", border:"1px solid #d5d5d5"}}>
                         <h4 className="text-2xl  text-lime-700" style={{fontFamily: 'SUITE-Regular'}}>
-                            <label for="number">휴대번호</label>
+                            <label for="e-mail">사원번호</label>
+                        </h4>
+                        <input className="w-64 h-10" style={{border:"1px solid #d5d5d5"}} disabled type="e-mail" id="e-mail"></input>
+                    </div>
+                    <div className="flex h-20 justify-around items-center rounded-2xl shadow-lg my-4" style={{backgroundColor:"#f6f5efb3", border:"1px solid #d5d5d5"}}>
+                        <h4 className="text-2xl  text-lime-700" style={{fontFamily: 'SUITE-Regular'}}>
+                            <label for="number">비밀번호</label>
                         </h4>
                         <input className="w-64 h-10" style={{border:"1px solid #d5d5d5"}} disabled type="text" id="number"></input>
                     </div>
 
-                    <div className="flex h-20 justify-around items-center rounded-2xl shadow-lg my-4" style={{backgroundColor:"#f6f5efb3", border:"1px solid #d5d5d5"}}>
-                        <h4 className="text-2xl  text-lime-700" style={{fontFamily: 'SUITE-Regular'}}>
-                            <button className="w-64 h-10" style={{border:"0px solid #d5d5d5"}}>비밀번호 변경</button>
-                        </h4>
-                        
+                    <div className="flex h-20 justify-around items-center rounded-2xl shadow-lg my-4"style={{backgroundColor:"#f6f5efb3", border:"1px solid #d5d5d5"}}>
+                        <div><button className="text-2xl  text-lime-700" style={{fontFamily: 'SUITE-Regular'}}>수정</button></div>
+                        <div><button className="text-2xl  text-lime-700" style={{fontFamily: 'SUITE-Regular'}}>뒤로가기</button></div>
                     </div>
 
 
                 </div>
             </div>
+
 
     
         
