a0809e36eff0d7a57f79f8348e598309651f5cdf
sw4689@naver.com <sw4689@naver.com>
2024-01-29 15:07:56 +0900

    feat : 점주용 내정보 수정

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -8,6 +8,8 @@ import { json, useLoaderData, useNavigate } from "react-router-dom";
 export default function MyPage() {
     const myData = useLoaderData();
     const [editPwd, setEditPwd] = useState(true);
+    const [checkPwd, setCheckPwd] = useState(""); 
+    const [checkPwdConfirm, setCheckPwdConfirm] = useState(""); 
 
     return (
         <>
@@ -16,84 +18,91 @@ export default function MyPage() {
 
 
 
-                <div className="w-5/12 h-fit mx-auto mt-16">
-                <div className="flex h-14 justify-around items-center  my-2" >
-                    <h4 className="text-3xl font-bold " style={{ fontFamily: "EASTARJET-Medium", textDecoration: "underline #eaeaea", textUnderlineOffset: "10px" }}>
-                        내 정보
-                    </h4>
-                </div>
-                    {/* 
-                    <h1 className="text-4xl font-semibold mx-auto w-fit text-lime-700" style={{ fontFamily: 'SUITE-Regular' }}>SSGTARBUCKS</h1> */}
-                    <div className="flex h-16 justify-between px-16 items-center rounded-lg shadow-lg my-4" style={{ backgroundColor: "#f6f5efb3", border: "1px solid #d5d5d5" }}>
-                        <h4 className="text-lg font-bold pl-4">
-                            <label for="id">지점코드</label>
+                <div className="w-8/12 h-fit mx-auto mt-10">
+                    <div className="text-center h-14 my-2" >
+                        <h4 className="text-3xl font-bold " style={{ fontFamily: "EASTARJET-Medium", textDecoration: "underline #eaeaea", textUnderlineOffset: "10px" }}>
+                            내 정보
                         </h4>
-                        <input className="w-96 h-10 border text-center text-xl" disabled type="text" id="id" value={myData.branch_id}></input>
+                    </div>
+                    <div >
+                        <div className="flex justify-around border-b my-8">
+                            <div className="">
+                                <div className="flex h-16 justify-between px-8 items-center rounded-lg shadow-lg my-4" style={{ backgroundColor: "#f6f5efb3", border: "1px solid #d5d5d5" }}>
+                                    <h4 className="text-lg font-bold mr-4">
+                                        <label for="id">지점코드</label>
+                                    </h4>
+                                    <input className="w-80 h-10 border text-center text-xl" disabled type="text" id="id" value={myData.branch_id}></input>
+                                </div>
+                                <div className="flex h-16 justify-between px-8 items-center rounded-lg shadow-lg my-4" style={{ backgroundColor: "#f6f5efb3", border: "1px solid #d5d5d5" }}>
+                                    <h4 className="text-lg font-bold mr-4">
+                                        <label for="id">지점명</label>
+                                    </h4>
+                                    <input className="w-80 h-10 border text-center text-xl" disabled type="text" id="id" value={myData.branch_name}></input>
+                                </div>
+                                <div className="flex h-16 justify-between px-8 items-center rounded-lg shadow-lg my-4" style={{ backgroundColor: "#f6f5efb3", border: "1px solid #d5d5d5" }}>
+                                    <h4 className="text-lg font-bold mr-4">
+                                        <label for="id">지점 주소</label>
+                                    </h4>
+                                    <input className="w-80 h-10 border text-center text-xl" disabled type="text" id="address" value={myData.branch_address.match(/([^(]*)/)[0].trim()}></input>
+                                </div>
+                                <div className="flex h-16 justify-between px-8 items-center rounded-lg shadow-lg my-4" style={{ backgroundColor: "#f6f5efb3", border: "1px solid #d5d5d5" }}>
+                                    <h4 className="text-lg font-bold mr-4">
+                                        <label for="id">지점 상세주소</label>
+                                    </h4>
+                                    <input className="w-80 h-10 border text-center text-xl" disabled type="text" id="address" value={myData.branch_address.match(/\(([^)]+)\)/)[1].trim()}></input>
+                                </div>
+                            </div>
 
-                    </div>
-                    <div className="flex h-16 justify-between px-16 items-center rounded-lg shadow-lg my-4" style={{ backgroundColor: "#f6f5efb3", border: "1px solid #d5d5d5" }}>
-                        <h4 className="text-lg font-bold pl-4">
-                            <label for="id">지점명</label>
-                        </h4>
-                        <input className="w-96 h-10 border text-center text-xl" disabled type="text" id="id" value={myData.branch_name}></input>
-
-                    </div>
-                    <div className="flex h-16 justify-between px-16 items-center rounded-lg shadow-lg my-4" style={{ backgroundColor: "#f6f5efb3", border: "1px solid #d5d5d5" }}>
-                        <h4 className="text-lg font-bold pl-4">
-                            <label for="id">지점 주소</label>
-                        </h4>
-                        <input className="w-96 h-10 border text-center text-xl" disabled type="text" id="address" value={myData.branch_address.match(/([^(]*)/)[0].trim()}></input>
-
-                    </div>
-                    <div className="flex h-16 justify-between px-16 items-center rounded-lg shadow-lg my-4" style={{ backgroundColor: "#f6f5efb3", border: "1px solid #d5d5d5" }}>
-                        <h4 className="text-lg font-bold pl-4">
-                            <label for="id">지점 상세주소</label>
-                        </h4>
-                        <input className="w-96 h-10 border text-center text-xl" disabled type="text" id="address" value={myData.branch_address.match(/\(([^)]+)\)/)[1].trim()}></input>
-
-                    </div>
-                    <div className="flex h-16 justify-between px-16 items-center rounded-lg shadow-lg my-4" style={{ backgroundColor: "#f6f5efb3", border: "1px solid #d5d5d5" }}>
-                        <h4 className="text-lg font-bold pl-4">
-                            <label for="number">이름</label>
-                        </h4>
-                        <input className="w-96 h-10 border text-center text-xl" disabled type="text" id="number" value={myData.user_name}></input>
-                    </div>
-                    <div className="flex h-16 justify-between px-16 items-center rounded-lg shadow-lg my-4" style={{ backgroundColor: "#f6f5efb3", border: "1px solid #d5d5d5" }}>
-                        <h4 className="text-lg font-bold pl-4">
-                            <label for="number">직원 코드</label>
-                        </h4>
-                        <input className="w-96 h-10 border text-center text-xl" disabled type="text" id="number" value={myData.user_id}></input>
-                    </div>
-                    <div className="flex h-16 justify-between px-16 items-center rounded-lg shadow-lg my-4" style={{ backgroundColor: "#f6f5efb3", border: "1px solid #d5d5d5" }}>
-                        <h4 className="text-lg font-bold pl-4">
-                            <label for="number">비밀번호</label>
-                        </h4>
-                        <input className="w-96 h-10 border text-center text-xl" disabled={editPwd} type="password" id="number" ></input>
-                    </div>
-                    {!editPwd &&
-                        <div className="flex h-16 justify-between px-16 items-center rounded-lg shadow-lg my-4" style={{ backgroundColor: "#f6f5efb3", border: "1px solid #d5d5d5" }}>
-                            <h4 className="text-lg font-bold pl-4">
-                                <label for="number">비밀번호 확인</label>
-                            </h4>
-                            <input className="w-96 h-10 border text-center text-xl" disabled={editPwd} type="password" id="number" ></input>
+                            <div>
+                                <div className="flex h-16 justify-between px-8 items-center rounded-lg shadow-lg my-4" style={{ backgroundColor: "#f6f5efb3", border: "1px solid #d5d5d5" }}>
+                                    <h4 className="text-lg font-bold mr-4">
+                                        <label for="number">이름</label>
+                                    </h4>
+                                    <input className="w-96 h-10 border text-center text-xl" disabled type="text" id="number" value={myData.user_name}></input>
+                                </div>
+                                <div className="flex h-16 justify-between px-8 items-center rounded-lg shadow-lg my-4" style={{ backgroundColor: "#f6f5efb3", border: "1px solid #d5d5d5" }}>
+                                    <h4 className="text-lg font-bold mr-4">
+                                        <label for="number">직원 코드</label>
+                                    </h4>
+                                    <input className="w-96 h-10 border text-center text-xl" disabled type="text" id="number" value={myData.user_id}></input>
+                                </div>
+                                <div className="flex h-16 justify-between px-8 items-center rounded-lg shadow-lg my-4" style={{ backgroundColor: "#f6f5efb3", border: "1px solid #d5d5d5" }}>
+                                    <h4 className="text-lg font-bold mr-4">
+                                        <label for="e-mail">이메일</label>
+                                    </h4>
+                                    <input className="w-96 h-10 border text-center text-xl" disabled type="e-mail" id="e-mail" value={myData.user_email}></input>
+                                </div>
+                                <div className="flex h-16 justify-between px-8 items-center rounded-lg shadow-lg my-4" style={{ backgroundColor: "#f6f5efb3", border: "1px solid #d5d5d5" }}>
+                                    <h4 className="text-lg font-bold mr-4">
+                                        <label for="number">연락처</label>
+                                    </h4>
+                                    <input className="w-96 h-10 border text-center text-xl" disabled type="text" id="number" value={myData.user_phone}></input>
+                                </div>
+                            </div>
                         </div>
-                    }
-                    <div className="flex h-16 justify-between px-16 items-center rounded-lg shadow-lg my-4" style={{ backgroundColor: "#f6f5efb3", border: "1px solid #d5d5d5" }}>
-                        <h4 className="text-lg font-bold pl-4">
-                            <label for="e-mail">이메일</label>
-                        </h4>
-                        <input className="w-96 h-10 border text-center text-xl" disabled type="e-mail" id="e-mail" value={myData.user_email}></input>
-                    </div>
 
-                    <div className="flex h-16 justify-between px-16 items-center rounded-lg shadow-lg my-4" style={{ backgroundColor: "#f6f5efb3", border: "1px solid #d5d5d5" }}>
-                        <h4 className="text-lg font-bold pl-4">
-                            <label for="number">연락처</label>
-                        </h4>
-                        <input className="w-96 h-10 border text-center text-xl" disabled type="text" id="number" value={myData.user_phone}></input>
+                        <div className="w-1/2 mx-auto">
+                            <div className="flex h-16 justify-between px-8 items-center rounded-lg shadow-lg my-4" style={{ backgroundColor: "#f6f5efb3", border: "1px solid #d5d5d5" }}>
+                                <h4 className="text-lg font-bold mr-4">
+                                    <label for="number">비밀번호</label>
+                                </h4>
+                                <input className="w-96 h-10 border text-center text-xl" disabled={editPwd} type="password" id="number" onInput={(e)=>setCheckPwd(e.target.value)}></input>
+                            </div>
+                            {!editPwd &&
+                                <div className="flex h-16 justify-between px-8 items-center rounded-lg shadow-lg my-4" style={{ backgroundColor: "#f6f5efb3", border: "1px solid #d5d5d5" }}>
+                                    <h4 className="text-lg font-bold mr-4">
+                                        <label for="number">비밀번호 확인</label>
+                                    </h4>
+                                    <input className="w-96 h-10 border text-center text-xl" disabled={editPwd} type="password" id="number" onInput={(e)=>setCheckPwdConfirm(e.target.value)}></input>
+                                </div>
+                            }
+                            {checkPwd && !editPwd && (checkPwd == checkPwdConfirm) && <h1 className="text-center text-lg">비밀번호가 일치합니다.</h1>}
+                        </div>
+
                     </div>
-                    <div className="flex h-20 justify-center items-center mt-16">
-                        <button className="text-lg page_itms h-2/3 w-1/4 border rounded-md shadow-md" onClick={() => setEditPwd(!editPwd)}>{editPwd ? "비밀번호 변경" : "변경하기"}</button>
-                        <button className="text-lg page_itms h-2/3 w-1/4 border rounded-md shadow-md" onClick={() => setEditPwd(!editPwd)}>{editPwd ? "취소" : "확인"}</button>
+                    <div className="flex h-20 justify-center items-center mt-14">
+                        <button className="text-lg page_itms h-2/3 w-1/4 border rounded-md shadow-md mx-3" onClick={() => setEditPwd(!editPwd)}>{editPwd ? "비밀번호 변경" : "변경하기"}</button>
+                        <button className="text-lg page_itms h-2/3 w-1/4 border rounded-md shadow-md mx-3" onClick={() => setEditPwd(!editPwd)}>{editPwd ? "취소" : "확인"}</button>
                     </div>
                 </div>
             </div>
